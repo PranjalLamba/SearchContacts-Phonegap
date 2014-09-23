b6ee4be35be1057a83a5dc0553a6905350bbea96
PranjalLamba <pranjallamba89@gmail.com>
2014-09-24 00:01:26 +0530

    Added Contacts API

--- SearchContacts-Phonegap/platforms/android/ant-build/classes.dex.d
+++ SearchContacts-Phonegap/platforms/android/ant-build/classes.dex.d
@@ -6,4 +6,13 @@ E:\Projects\GitHub\SearchContacts-Phonegap\SearchContacts-Phonegap\platforms\and
 E:\Projects\GitHub\SearchContacts-Phonegap\SearchContacts-Phonegap\platforms\android\ant-build\classes\com\app\SearchContacts\R$xml.class \
 E:\Projects\GitHub\SearchContacts-Phonegap\SearchContacts-Phonegap\platforms\android\ant-build\classes\com\app\SearchContacts\R.class \
 E:\Projects\GitHub\SearchContacts-Phonegap\SearchContacts-Phonegap\platforms\android\ant-build\classes\com\app\SearchContacts\SearchContacts.class \
+E:\Projects\GitHub\SearchContacts-Phonegap\SearchContacts-Phonegap\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactAccessor$WhereOptions.class \
+E:\Projects\GitHub\SearchContacts-Phonegap\SearchContacts-Phonegap\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactAccessor.class \
+E:\Projects\GitHub\SearchContacts-Phonegap\SearchContacts-Phonegap\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactAccessorSdk5.class \
+E:\Projects\GitHub\SearchContacts-Phonegap\SearchContacts-Phonegap\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactInfoDTO.class \
+E:\Projects\GitHub\SearchContacts-Phonegap\SearchContacts-Phonegap\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactManager$1.class \
+E:\Projects\GitHub\SearchContacts-Phonegap\SearchContacts-Phonegap\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactManager$2.class \
+E:\Projects\GitHub\SearchContacts-Phonegap\SearchContacts-Phonegap\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactManager$3.class \
+E:\Projects\GitHub\SearchContacts-Phonegap\SearchContacts-Phonegap\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactManager$4.class \
+E:\Projects\GitHub\SearchContacts-Phonegap\SearchContacts-Phonegap\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactManager.class \
 E:\Projects\GitHub\SearchContacts-Phonegap\SearchContacts-Phonegap\platforms\android\ant-build\dexedLibs\classes-588aaf600e1f6599ed4d7f10a98f505b.jar \
